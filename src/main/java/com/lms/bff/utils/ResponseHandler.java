5a36b0dc532b0d9e04bbf17d0a31cc94c8dd10c0
Nagendra Anala <Nagendraanala@gmail.com>
2025-05-06 23:19:58 +0530

    Migrating from jdbc to r2dbc

--- src/main/java/com/lms/bff/utils/ResponseHandler.java
+++ src/main/java/com/lms/bff/utils/ResponseHandler.java
@@ -2,6 +2,7 @@ package com.lms.bff.utils;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import reactor.core.publisher.Mono;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -26,5 +27,21 @@ public class ResponseHandler {
     response.put("httpStatus", httpStatus);
 
     return new ResponseEntity<>(response, httpStatus);
+  }
+
+  public static ResponseEntity<Object> responseBuilder(
+      Object responseObject
+  ){
+    Map<String, Object> response = new HashMap<>();
+    response.put("data", responseObject);
+
+    return new ResponseEntity<>(response, HttpStatus.OK);
+  }
+
+  public static Mono<ResponseEntity<Object>> finalResponse(
+       Object responseObject
+  ){
+    ResponseEntity<Object> resp = ResponseHandler.responseBuilder(responseObject);
+    return Mono.just(resp);
   }
 }
